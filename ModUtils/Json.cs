9831396aaf865ff33be1ad17d34bb60b9b6fe30e
eideehi <41231321+eideehi@users.noreply.github.com>
2022-12-16 21:04:19 +0900

    Add functions to the Json utility
    Add functions to convert an object to JSON string.
    Rename structure.

--- ModUtils/Json.cs
+++ ModUtils/Json.cs
@@ -1,35 +1,61 @@
-﻿using LitJson;
+﻿using System.Text;
+using LitJson;
 
 namespace ModUtils
 {
     public static class Json
     {
-        public static T Parse<T>(string jsonText, ParserOption option)
+        public static T Parse<T>(string jsonText, ReaderOption option)
         {
-            var reader = new JsonReader(jsonText)
+            return JsonMapper.ToObject<T>(new JsonReader(jsonText)
             {
                 AllowComments = option.AllowComments,
                 AllowSingleQuotedStrings = option.AllowSingleQuotedStrings,
                 SkipNonMembers = option.SkipNonMembers
-            };
-            return JsonMapper.ToObject<T>(reader);
+            });
         }
 
         public static T Parse<T>(string jsonText)
         {
-            return Parse<T>(jsonText, new ParserOption
+            return Parse<T>(jsonText, new ReaderOption
             {
                 AllowComments = true,
                 AllowSingleQuotedStrings = false,
                 SkipNonMembers = true
             });
         }
+
+        public static string ToString(object obj, WriterOption option)
+        {
+            var stringBuilder = new StringBuilder();
+            JsonMapper.ToJson(obj, new JsonWriter(stringBuilder)
+            {
+                IndentValue = option.IndentSize,
+                PrettyPrint = option.Pretty
+            });
+            return stringBuilder.ToString();
+        }
+
+        public static string ToString(object obj)
+        {
+            return ToString(obj, new WriterOption
+            {
+                Pretty = false,
+                IndentSize = 0
+            });
+        }
     }
 
-    public struct ParserOption
+    public struct ReaderOption
     {
         public bool AllowComments;
         public bool AllowSingleQuotedStrings;
         public bool SkipNonMembers;
+    }
+
+    public struct WriterOption
+    {
+        public bool Pretty;
+        public int IndentSize;
     }
 }
